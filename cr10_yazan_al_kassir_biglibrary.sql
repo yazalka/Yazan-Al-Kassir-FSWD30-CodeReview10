f82627ccf4e52fa209154dab7c0e9cb9ab762c00
Yazan Alkassir <yazan.kassir1@gmail.com>
2018-02-10 16:38:06 +0100

    Databasse Last Version

--- cr10_yazan_al_kassir_biglibrary.sql
+++ cr10_yazan_al_kassir_biglibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2018 at 05:30 PM
+-- Generation Time: Feb 10, 2018 at 04:36 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -75,7 +75,7 @@ CREATE TABLE `media` (
 INSERT INTO `media` (`media_id`, `type`, `title`, `image`, `short_description`, `publish_date`, `fk_author_id`, `fk_publisher_id`, `isbn_code`) VALUES
 (1, 'book', ' Anna Karenina', 'http://ecx.images-amazon.com/images/I/51vPf2CfSEL._SL160_.jpg', 'Anna Karenina tells of the doomed love affair between the sensuous and rebellious Anna and the dashing officer, Count Vronsky. Tragedy unfolds as Anna rejects her passionless marriage and must endu.', '2015-03-05', 1, 1, 5684368965432),
 (2, 'book', 'Madame Bovary', 'http://ecx.images-amazon.com/images/I/51o5dnjk07L._SL160_.jpg', 'For daring to peer into the heart of an adulteress and enumerate its contents with profound dispassion, the author of Madame Bovary was tried for offenses against morality and religion. What shoc', '2015-05-04', 2, 2, 5684653742432),
-(3, 'book', 'The Great Gatsby', 'http://ecx.images-amazon.com/images/I/51o5dnjk07L._SL160_.jpg', 'The novel chronicles an era that Fitzgerald himself dubbed the \"Jazz Age\". Following the shock and chaos of World War I, American society enjoyed unprecedented levels of prosperity during the', '2013-07-26', 3, 3, 7534765982345),
+(3, 'book', 'The Great Gatsby', 'https://images-na.ssl-images-amazon.com/images/I/51khWutZqCL._SL160_.jpg', 'The novel chronicles an era that Fitzgerald himself dubbed the \"Jazz Age\". Following the shock and chaos of World War I, American society enjoyed unprecedented levels of prosperity during the', '2013-07-26', 3, 3, 7534765982345),
 (4, 'book', ' Lolita', 'https://images-na.ssl-images-amazon.com/images/I/41s2G6WxLvL._SL160_.jpg', 'The book is internationally famous for its innovative style and infamous for its controversial subject: the protagonist and unreliable narrator, middle aged Humbert Humbert, becomes obsessed and se', '2012-06-24', 4, 4, 5684654567842),
 (5, 'book', 'The Adventures of Huckleberry Fin', 'http://ecx.images-amazon.com/images/I/51Ht1M-GPXL._SL160_.jpg', 'Revered by all of the towns children and dreaded by all of its mothers, Huckleberry Finn is indisputably the most appealing child-hero in American literature. Unlike the tall-tale, idyllic worl.', '2011-01-04', 5, 5, 1256783742432),
 (6, 'book', 'Middlemarch', 'http://ecx.images-amazon.com/images/I/51eRjEMiOgL._SL160_.jpg', 'Middlemarch: A Study of Provincial Life is a novel by George Eliot, the pen name of Mary Anne Evans, later Marian Evans. It is her seventh novel, begun in 1869 and then put aside during the final', '2014-06-15', 6, 6, 1234567543212),
@@ -126,6 +126,14 @@ CREATE TABLE `users` (
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`users_id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'Yazan', 'Alkassir', 'yazan.kasser1@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+(2, 'yazan', 'alkassir', 'yazan.kassir1@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225');
 
 --
 -- Indexes for dumped tables
@@ -183,7 +191,7 @@ ALTER TABLE `publisher`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
